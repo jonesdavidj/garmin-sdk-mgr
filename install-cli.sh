6304ccc1d1b65bcda7c8a30f4d080e159c28f1d7
jonesdavidj <45854802+jonesdavidj@users.noreply.github.com>
2025-06-13 23:50:27 +0100

    bug: remove-sudo-privs

--- install-cli.sh
+++ install-cli.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
 set -e
 
 echo "📦 Installing Connect IQ SDK Manager CLI..."
-curl -s https://raw.githubusercontent.com/lindell/connect-iq-sdk-manager-cli/master/install.sh -o /tmp/install.sh
+curl -s https://raw.githubusercontent.com/lindell/connect-iq-sdk-manager-cli/master/install.sh \
+  | sed 's/sudo //' \
+  > /tmp/install.sh
+
 bash /tmp/install.sh
 rm /tmp/install.sh
 
-echo "✅ CLI installed. Now run ./sdk-init.sh to continue setup."
+echo "✅ CLI installed. Now run sdk-init.sh to complete setup."
